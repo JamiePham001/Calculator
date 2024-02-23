66953fb1925823d22793b8a202c5a7482a97b344
Jamie Pham <jamie.pham@outlook.com>
2024-02-23 21:46:22 +0800

    complete project
    - clear button fully resets calculator
    - able to string together multi digit integers.
    -able to string to together series of operators and integers.
    - added rounding by one decimal place
    - fixed spelling on html file

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 // add
-const add = (a,b) => {
-    return a+b;
-}
+const add = (a, b) => {
+    const num1 = parseInt(a);
+    const num2 = parseInt(b);
+    const total = num1 + num2;
+
+    return Math.round(total * 10) / 10;
+};
+
 
 // subtract
 const sub = (a,b) => {
-    return a-b;
+    const total = a - b;
+    return Math.round(total * 10) / 10;
 }
 
 // multiply
 const multi = (a,b) => {
-    return a*b;
+    const total = a * b;
+    return Math.round(total * 10) / 10;
 }
 
 // divide
@@ -34,27 +41,71 @@ const operate = (operator,a,b) => {
     }
 }
 
-// document.addEventListener('DOMContentLoaded', () => {
+let currentNumber = 0;
+let nextNumber = 0;
+let operationSelect = '';
 
-// })
+let numButtons = document.querySelectorAll('.number');
+let opButtons = document.querySelectorAll('.operator');
+const clear = document.querySelector('.clear');
+const decimal = document.querySelector('.decimal');
 
-let buttons = document.querySelectorAll('.number');
-const clear = document.querySelector('.clear')
+let isNewNumber = true;
+let isNextNumber = false;
 
-buttons.forEach(button => {
+numButtons.forEach(button => {
     button.addEventListener('click', function(){
         const screen = document.querySelector('.screen');
-        const currentContent = screen.textContent;
-        const newContent = currentContent === '0' ? button.textContent : currentContent + button.textContent;
-        screen.textContent = newContent;
+        let currentContent = screen.textContent;
+
+        // if statement allows for the ability to string numbers together in succession
+        if (isNewNumber) { // if it is a new number, can enter a fresh number
+            screen.textContent = button.textContent;
+            isNewNumber = false; // set false so more than 2 digit integers can be made
+            console.log(isNewNumber)
+        } else { // when false, allows user to string together multi digit numbers.
+            const newContent = currentContent === '0' ? button.textContent : currentContent + button.textContent;
+            screen.textContent = newContent;
+            decimal.disabled = true;
+        }
+
       })
    })
 
-clear.addEventListener('click', function(){
-    document.querySelector('.screen').textContent = 0;
+opButtons.forEach(button => {
+    button.addEventListener('click', function(){
+        // on operator button click save current number into a variable
+        const screen = document.querySelector('.screen');
+        let currentContent = screen.textContent;
+
+        if (!isNextNumber) {
+            // If the user is continuing the current number, set currentNumber
+            currentNumber = currentContent;
+            isNextNumber = true;
+            decimal.disabled = false;
+        } else {
+            // while isNextNumber is true, set the nextNumber and perform the operate function
+            nextNumber = currentContent;
+            let calc = operate(operationSelect, currentNumber, nextNumber)
+            screen.textContent = calc;
+            currentNumber = calc;
+            console.log('calc', calc)
+        }
+
+        // set operater value to variabe and reset screen
+        operationSelect = button.value;
+        isNewNumber = true;
+      })
 })
 
-// store number inputs as variables. i.e, num1, num2
-// use the operate function when user clicks the operator button
-// disable button upon pressing an operator button. Some exceptions are the subtract operator to allow for negative numbers during multiplication...
-// on press of multiplication display the 'x' symbol and in the actual calculation use '*'
+// event listener for clear button
+clear.addEventListener('click', function(){
+    // clear screen and revert all values to default
+    document.querySelector('.screen').textContent = 0;
+    currentNumber = 0;
+    nextNumber = 0;
+    operationSelect = '';
+    isNewNumber = true;
+    isNextNumber = false;
+    decimal.disabled = false;
+})
